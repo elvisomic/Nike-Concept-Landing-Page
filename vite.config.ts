32da6b1e6ec3d804035060a0b745c2bcf4aa05e1
Elvis Omić <elvis08.eo@gmail.com>
2025-10-13 00:00:49 +0200

    gh-pages fix 1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Nike-Concept-Landing-Page/',
+  base: '/Nike-Concept-Landing-Page',
   plugins: [react(), tailwindcss()],
 })
